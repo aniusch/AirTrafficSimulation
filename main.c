518189fbeda278b87a3affcda2fa572f9920bde9
Gabriel Schuina <gabrielschuina86@gmail.com>
2023-06-04 20:13:15 -0400

    remove random time seed

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 int main(int argc, char* argv[]){
     int n;
     n = atoi(argv[1]);
-    srand(time(NULL)); // Inicializa a semente para geração de números aleatórios
     if(n > MAX){
         printf("O valor de n deve ser menor ou igual a %d\n", MAX);
         return 1;
